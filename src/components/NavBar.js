c171f7729d01516483a9272005e15b1e596087a2
Abhay Jirati <abhi.jrt12@gmail.com>
2020-09-23 19:32:29 +0530

    adding the navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,45 @@
+import React from "react";
+// import { AppBar, Tabs, Tab } from "material-ui";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Link from "@material-ui/core/Link";
+
+export default function () {
+  return (
+    <React.Fragment>
+      <AppBar position="static">
+        <Toolbar>
+          <h1>Task Manager</h1>
+          <ul style={styles.listContainer}>
+            <li style={styles.listItem}>
+              <Link style={styles.link}>Home</Link>
+            </li>
+            <li style={styles.listItem}>
+              <Link style={styles.link}>Task</Link>
+            </li>
+            <li style={styles.listItem}>
+              <Link style={styles.link}>User</Link>
+            </li>
+          </ul>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+}
+
+const styles = {
+  listContainer: {
+    listStyle: "none",
+    display: "flex",
+    flexDirection: "row",
+  },
+  listItem: {
+    fontSize: "30px",
+    marginLeft: "20px",
+  },
+  link: {
+    color: "white",
+    cursor: "pointer",
+    textDecoration: "none",
+  },
+};
